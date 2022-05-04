de67f77b463d570c12c7d4ee6bf4fb8bf89a474f
sofiaaaltamiranob <sofiaaaltamiranob@gmail.com>
2022-05-04 10:43:16 -0300

    primer commit angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+}
+
+  /* Forma larga y separada de hacerlo
+  sumar() {
+    this.numero += 1;
+  }
+  restar(){
+    this.numero -= 1;
+  } */
+
+
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad: number = 42;
+
+    get nombreCapitalizado(): string{
+        return this.nombre.toUpperCase();
+    }
+    obtenerNombre(): string {
+
+        return `${ this.nombre } - ${ this.edad}`;
+        /*esto sería lo mismo que hacer:
+        return this.nombre + ' - ' + this.edad.toString();*/
+
+    }
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+    cambiarEdad(): void{
+        this.edad = 30;
+    }
+}
